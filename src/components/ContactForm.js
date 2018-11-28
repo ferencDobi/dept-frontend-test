79b5bb5f3a7d80c26f8b19fc3676c17f85b26724
ferencDobi <ferencdbi@gmail.com>
2018-11-28 11:23:57 +0100

    implement contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,9 +1,24 @@
 import React from "react";
 
+const text = `Question?  We are here to help!`;
+
 export const ContactForm = () => {
   return (
-      <div>
-
+      <div className="contact-form">
+        <h1>{text}</h1>
+        <div className="name-field">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" />
+        </div>
+        <div className="email-field">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email "/>
+        </div>
+        <div className="message-box">
+          <label htmlFor="message">Message</label>
+          <textarea name="message" cols="32" rows="6" />
+        </div>
+        <button>Send</button>
       </div>
   );
 };
