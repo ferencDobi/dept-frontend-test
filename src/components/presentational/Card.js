cf484de828a3be3e320218f1250871364c12bedc
ferencDobi <ferencdbi@gmail.com>
2018-11-28 20:47:49 +0100

    fallback image and unicode quickfix

--- src/components/presentational/Card.js
+++ src/components/presentational/Card.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+const fallbackImage = 'https://picsum.photos/1400/940/?random';
+
 export const Card = ({article}) => {
   return (
       <div>
-        <img src={article.image} alt="" />
+        <img src={article.image} alt="" onError={event => event.target.src = fallbackImage} />
         <h3>{article.name}</h3>
         <h1>{article.title}</h1>
         <button>View Case</button>
